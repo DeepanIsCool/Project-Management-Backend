7d008f7ad37cb1de0a81d1e29d54176f4e86464b
DeepanIsCool <deepan.sadhukhan2023@iem.edu.in>
2024-07-03 16:15:29 +0530

    refactor: Add ValidateEmailOTP route to studentsRoutes.js

--- routes/studentsRoutes.js
+++ routes/studentsRoutes.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const { Sign_upvalidation } = require('../controllers/authController');
 const { Sign_up } = require('../controllers/authController');
+const { ValidateEmailOTP } = require('../controllers/authController');
 const router = express.Router();
 
 router.route("/signup").post(Sign_up);
 router.route("/getStudentData").post(Sign_upvalidation);
+router.route("/ValidateEmailOtp").post(ValidateEmailOTP);
 
 module.exports = router;
