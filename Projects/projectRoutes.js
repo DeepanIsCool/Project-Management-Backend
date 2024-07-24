f91e948fd2dcbbd12abfa7268878c7e1a55e1340
Deepan Sadhukhan <deepan.sadhukhan2023@iem.edu.in>
2024-07-24 11:10:39 +0530

    chore: Update projectRoutes.js to include getProjects route for students

--- Projects/projectRoutes.js
+++ Projects/projectRoutes.js
@@ -7,7 +7,8 @@ const { studentsChecker } = require("../Students/studentsChecker");
 
 router.route("/createProject").post(facultyChecker,CreateProject);
 router.route("/editProject/:id").put(facultyChecker,EditProject);
-router.route("/getProject").get(facultyChecker,getAllProjects);
+router.route("/getProjects").get(facultyChecker,getAllProjects);
+router.route("/getProjects").get(studentsChecker,getAllProjects);
 router.route("/applyforProject/:id").post(studentsChecker,applyForProject);
 router.route("/approveforProject/:id").post(approveApplication);
 
